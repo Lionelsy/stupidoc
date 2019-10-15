6fe7fd1be416be2534c54a9e46a4fd98e649b6f1
qiuqi929 <qiuqi929@gmail.com>
2019-10-15 19:15:07 +0800

    Framework_commit
    Commit the framework, and test the environment configuration of gradlew.

---
+++ src/main/java/org/sustech/stupidoc/StupidocApplication.java
@@ -0,0 +1,13 @@
+package org.sustech.stupidoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StupidocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StupidocApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/sustech/stupidoc/Test.java
@@ -0,0 +1,8 @@
+package org.sustech.stupidoc;
+
+public class Test {
+
+    static void hello(){
+        System.out.println("Hello world");
+    }
+}
